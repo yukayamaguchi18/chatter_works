b0bbf1f808af351db9a682b30de29197d3d4879f
yukayamaguchi18 <141097661+yukayamaguchi18@users.noreply.github.com>
2023-10-25 06:02:32 +0000

    [Fix]follow tag

--- app/models/user.rb
+++ app/models/user.rb
@@ -125,7 +125,7 @@ class User < ApplicationRecord
   # selfにcurrent_userを指定
   def save_tag(sent_tags)
     # タグが存在していれば、タグの名前を配列として全て取得
-      current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
+      current_tags = self.tags.pluck(:name) unless self.tags.nil?
       # 現在取得したタグから送られてきたタグを除いてoldtagとする
       old_tags = current_tags - sent_tags
       # 送信されてきたタグから現在存在するタグを除いたタグをnewとする
