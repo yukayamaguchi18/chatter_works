684c6f966cc36e104fad3f6d00582a964dcac478
yukayamaguchi18 <141097661+yukayamaguchi18@users.noreply.github.com>
2023-11-09 11:55:39 +0000

    [Add]tab menu design

---
+++ app/javascript/tab-icon.js
@@ -0,0 +1,8 @@
+// 実装保留 タブ内アイコンのactive,passiveの塗りつぶし切替用
+
+// const SOLID_CLASS = "fa-solid"  // 塗りつぶしたアイコンクラス
+// const REGULAR_CLASS = "fa-regular" // 外枠のみのアイコンクラス
+
+// $(".nav-link").on("click", function(){
+//   $(".nav-link i").toggleClass("SOLID_CLASS REGULAR_CLASS");
+// });
