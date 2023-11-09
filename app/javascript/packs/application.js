760dcbd3cf20efef1ae294ec470b3413d0c2259c
yukayamaguchi18 <141097661+yukayamaguchi18@users.noreply.github.com>
2023-11-09 05:50:21 +0000

    [Fix]responsive-tab.js to js dir

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,9 +12,9 @@ import "jquery";
 import JQuery from 'jquery'; //create.js.erb等で.modal functionを読み込むために追加
 window.$ = window.JQuery = JQuery; //create.js.erb等で.modal functionを読み込むために追加
 import "popper.js";
+import "responsive-tab.js"; //homes#top,keyword searchページでのレスポンシブタブ表示用に追加
 import "bootstrap";
 import "../stylesheets/application";
-import "responsive-tab.js"; //homes#top,keyword searchページでのレスポンシブタブ表示用に追加
 
 Rails.start()
 Turbolinks.start()
