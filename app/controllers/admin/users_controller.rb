7446a4951ab07220499aa6676694c97f81cd1db6
yukayamaguchi18 <141097661+yukayamaguchi18@users.noreply.github.com>
2023-11-23 03:44:38 +0000

    [Fix]Admin error

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,9 +8,9 @@ class Admin::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @chatters = @user.chatters_with_rechatters.page(params[:page]).per(20)
-    @works = @user.works.with_attached_work_image.order(created_at: :desc).page(params[:page]).per(10)
+    @works = @user.works.with_attached_work_images.order(created_at: :desc).page(params[:page]).per(10)
     @chatter_favorites = @user.chatter_favorites.includes([:chatter, :chatter_user]).order(created_at: :desc).page(params[:page]).per(20)
-    @work_favorites = @user.work_favorites.includes([:work, :work_image, :work_user]).order(created_at: :desc).page(params[:page]).per(10)
+    @work_favorites = @user.work_favorites.includes([:work, :work_user]).order(created_at: :desc).page(params[:page]).per(10)
     return unless request.xhr?
     case params[:type]
     when 'chatter', 'work', 'chatter_favorite', 'work_favorite'
